c137bd0334ace4f47ad52022ac3c46af098b40c7
PoomNattapoom <rockyaggi@gmail.com>
2023-07-17 16:34:24 +0700

    finish

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -2,7 +2,7 @@ export const Footer = (props) => {
   return (
     <div className="mt-3">
       <p className="text-secondary text-center">
-        {props.year} {props.fullName} {props.studentId}
+        copyright © {props.year} {props.fullName} {props.studentId}
       </p>
     </div>
   );
